8cfa82fe8f6a4716ff0ad4d3d8f18a6ea541ccc6
Kmahamadou <kmahamadou01@gmail.com>
2024-06-15 23:00:10 +0000

    Updated Banner image alt text with a more meaningful text for a better screen-reader experience

--- src/shared/Banner.jsx
+++ src/shared/Banner.jsx
@@ -16,7 +16,7 @@ const Banner = ({banner, heading, subheading, btn1, btn2}) => {
                         whileInView={'show'}
                         viewport={{once:false, amount:0.2}}
                         >
-                            <img src={banner} className='lg:h-[386px]' alt="banner image" />
+                            <img src={banner} className='lg:h-[386px]' alt="Un élève qui s'offre une session de lecture" />
                         </motion.div>
 
                         {/* Banner content */}
